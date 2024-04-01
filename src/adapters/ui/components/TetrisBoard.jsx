afca2e3ce72aa92968518ecf09c924622af7d989
obed-tc <castroobed53@gmail.com>
2024-03-31 22:49:07 -0400

    feat: Added full scoring and line removal

--- src/adapters/ui/components/TetrisBoard.jsx
+++ src/adapters/ui/components/TetrisBoard.jsx
@@ -1,51 +1,50 @@
-import React, { useEffect,useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from "react";
 
 const TetrisBoard = () => {
   const canvasRef = useRef(null);
 
-  // Ejemplo de un array 2D para representar el estado del tablero (0 para celdas vacías, 1 para celdas ocupadas)
-    const [tetrisBoard, setTetrisBoard] = useState([
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0, 0, 1, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-
+  const [tetrisBoard, setTetrisBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const [currentPiece, setCurrentPiece] = useState({
     shape: [
-        [1, 1,1], 
-        [0, 1,0]
+      [1, 1, 1],
+      [0, 1, 0],
     ], // Ejemplo de pieza cuadrada
     position: { x: 7, y: 0 }, // Posición inicial de la pieza
   });
-
-  const cellSize = 15;
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const cellSize = 30;
   const drawBoard = () => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Dibujar el tablero
     for (let row = 0; row < tetrisBoard.length; row++) {
       for (let col = 0; col < tetrisBoard[row].length; col++) {
         const cellValue = tetrisBoard[row][col];
-        ctx.fillStyle = cellValue === 1 ? 'yellow' : 'black';
+        ctx.fillStyle = cellValue === 1 ? "yellow" : "black";
         ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
-        ctx.strokeStyle = 'transparent';
+        ctx.strokeStyle = "transparent";
         ctx.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
       }
     }
@@ -57,34 +56,34 @@ const TetrisBoard = () => {
         if (cellValue === 1) {
           const x = currentPiece.position.x + col;
           const y = currentPiece.position.y + row;
-          ctx.fillStyle = 'yellow';
+          ctx.fillStyle = "yellow";
           ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
         }
       }
     }
   };
-  
+
   const handleKeyDown = (event) => {
     switch (event.key) {
-      case 'ArrowLeft':
+      case "ArrowLeft":
         movePieceLeft();
         break;
-      case 'ArrowRight':
+      case "ArrowRight":
         movePieceRight();
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
         movePieceDown();
         break;
       default:
         break;
     }
   };
+
   const movePieceDown = () => {
     setCurrentPiece((prevPiece) => ({
       ...prevPiece,
       position: { ...prevPiece.position, y: prevPiece.position.y + 1 },
     }));
-      
   };
 
   const movePieceLeft = () => {
@@ -100,32 +99,35 @@ const TetrisBoard = () => {
       position: { ...prevPiece.position, x: prevPiece.position.x + 1 },
     }));
   };
+
   const handleCollision = (newPiecePosition) => {
     const newTetrisBoard = tetrisBoard.map((row, rowIndex) =>
-      rowIndex < newPiecePosition.y || rowIndex >= newPiecePosition.y + currentPiece.shape.length
+      rowIndex < newPiecePosition.y ||
+      rowIndex >= newPiecePosition.y + currentPiece.shape.length
         ? [...row] // Copia las filas que no están afectadas por la nueva pieza
         : row.map((cell, colIndex) =>
-            colIndex >= newPiecePosition.x && colIndex < newPiecePosition.x + currentPiece.shape[0].length
-              ? currentPiece.shape[rowIndex - newPiecePosition.y][colIndex - newPiecePosition.x] || cell
+            colIndex >= newPiecePosition.x &&
+            colIndex < newPiecePosition.x + currentPiece.shape[0].length
+              ? currentPiece.shape[rowIndex - newPiecePosition.y][
+                  colIndex - newPiecePosition.x
+                ] || cell
               : cell
           )
     );
-  
+
     setTetrisBoard(newTetrisBoard);
     setCurrentPiece(getRandomPiece());
+    removeCompletedLines(newTetrisBoard);
   };
 
   const checkCollision = (piece, position, board) => {
     return piece.shape.some((row, rowIndex) =>
       row.some((cell, colIndex) => {
         if (cell) {
-          const boardRow = position.y + rowIndex+1;
+          const boardRow = position.y + rowIndex + 1;
           const boardCol = position.x + colIndex;
           // Verificar colisiones con el borde inferior o celdas fijas en el tablero
-          if (
-            boardRow >= board.length ||
-            board[boardRow][boardCol]
-          ) {
+          if (boardRow >= board.length || board[boardRow][boardCol]) {
             return true;
           }
         }
@@ -133,33 +135,63 @@ const TetrisBoard = () => {
       })
     );
   };
+
   const getRandomPiece = () => {
     const pieces = [
       {
         shape: [
           [1, 1, 1],
-          [0, 1, 0]
+          [0, 1, 0],
         ],
       },
       {
         shape: [
           [1, 1],
-          [1, 1]
+          [1, 1],
         ],
+      },
+      {
+        shape: [
+          [0, 1, 0],
+          [1, 1, 1],
+        ],
+      },
+      {
+        shape: [[1], [1], [1]],
       },
       // ... Agrega más tipos de piezas según tus necesidades
     ];
-  
+
     const randomPieceIndex = Math.floor(Math.random() * pieces.length);
     const newPiece = pieces[randomPieceIndex];
-  
+
     // Posición inicial aleatoria en la parte superior del tablero
     const initialPosition = {
-      x: Math.floor(Math.random() * (tetrisBoard[0].length - newPiece.shape[0].length + 1)),
+      x: Math.floor(
+        Math.random() * (tetrisBoard[0].length - newPiece.shape[0].length + 1)
+      ),
       y: 0,
     };
-  
+
     return { shape: newPiece.shape, position: initialPosition };
+  };
+
+  const removeCompletedLines = (board) => {
+    let newBoard = board.slice();
+    let linesCleared = 0;
+
+    for (let row = newBoard.length - 1; row >= 0; row--) {
+      if (newBoard[row].every((cell) => cell === 1)) {
+        newBoard.splice(row, 1);
+        newBoard.unshift(new Array(board[0].length).fill(0));
+        linesCleared++;
+      }
+    }
+
+    if (linesCleared > 0) {
+      setScore((prevScore) => prevScore + linesCleared * 100);
+      setTetrisBoard(newBoard);
+    }
   };
 
   useEffect(() => {
@@ -170,7 +202,11 @@ const TetrisBoard = () => {
       };
 
       // Verificar colisiones con el tablero
-      const pieceCollides = checkCollision(currentPiece, newPiecePosition, tetrisBoard);
+      const pieceCollides = checkCollision(
+        currentPiece,
+        newPiecePosition,
+        tetrisBoard
+      );
 
       if (!pieceCollides) {
         setCurrentPiece((prevPiece) => ({
@@ -186,22 +222,243 @@ const TetrisBoard = () => {
       clearInterval(intervalId);
     };
   }, [tetrisBoard, currentPiece]);
+
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
+
   useEffect(() => {
     drawBoard();
-    
-    
   }, [tetrisBoard, currentPiece]);
-  
-  
-  return <canvas ref={canvasRef} width={270} height={300} className="border-2 border-gray-800"></canvas>;
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={500}
+        height={550}
+        className="border-2 border-gray-800"
+      ></canvas>
+      <div>Score: {score}</div>
+    </div>
+  );
 };
 
 export default TetrisBoard;
 
+// import React, { useEffect, useState, useRef } from "react";
 
+// const TetrisBoard = () => {
+//   const canvasRef = useRef(null);
+
+//   const [tetrisBoard, setTetrisBoard] = useState([
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+//   ]);
+//   const [currentPiece, setCurrentPiece] = useState({
+//     shape: [
+//       [1, 1, 1],
+//       [0, 1, 0],
+//     ], // Ejemplo de pieza cuadrada
+//     position: { x: 7, y: 0 }, // Posición inicial de la pieza
+//   });
+
+//   const cellSize = 15;
+//   const drawBoard = () => {
+//     const canvas = canvasRef.current;
+//     const ctx = canvas.getContext("2d");
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     // Dibujar el tablero
+//     for (let row = 0; row < tetrisBoard.length; row++) {
+//       for (let col = 0; col < tetrisBoard[row].length; col++) {
+//         const cellValue = tetrisBoard[row][col];
+//         ctx.fillStyle = cellValue === 1 ? "yellow" : "black";
+//         ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+//         ctx.strokeStyle = "transparent";
+//         ctx.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
+//       }
+//     }
+
+//     // Dibujar la pieza actual
+//     for (let row = 0; row < currentPiece.shape.length; row++) {
+//       for (let col = 0; col < currentPiece.shape[row].length; col++) {
+//         const cellValue = currentPiece.shape[row][col];
+//         if (cellValue === 1) {
+//           const x = currentPiece.position.x + col;
+//           const y = currentPiece.position.y + row;
+//           ctx.fillStyle = "yellow";
+//           ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+//         }
+//       }
+//     }
+//   };
+
+//   const handleKeyDown = (event) => {
+//     switch (event.key) {
+//       case "ArrowLeft":
+//         movePieceLeft();
+//         break;
+//       case "ArrowRight":
+//         movePieceRight();
+//         break;
+//       case "ArrowDown":
+//         movePieceDown();
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+//   const movePieceDown = () => {
+//     setCurrentPiece((prevPiece) => ({
+//       ...prevPiece,
+//       position: { ...prevPiece.position, y: prevPiece.position.y + 1 },
+//     }));
+//   };
+
+//   const movePieceLeft = () => {
+//     setCurrentPiece((prevPiece) => ({
+//       ...prevPiece,
+//       position: { ...prevPiece.position, x: prevPiece.position.x - 1 },
+//     }));
+//   };
+
+//   const movePieceRight = () => {
+//     setCurrentPiece((prevPiece) => ({
+//       ...prevPiece,
+//       position: { ...prevPiece.position, x: prevPiece.position.x + 1 },
+//     }));
+//   };
+//   const handleCollision = (newPiecePosition) => {
+//     const newTetrisBoard = tetrisBoard.map((row, rowIndex) =>
+//       rowIndex < newPiecePosition.y ||
+//       rowIndex >= newPiecePosition.y + currentPiece.shape.length
+//         ? [...row] // Copia las filas que no están afectadas por la nueva pieza
+//         : row.map((cell, colIndex) =>
+//             colIndex >= newPiecePosition.x &&
+//             colIndex < newPiecePosition.x + currentPiece.shape[0].length
+//               ? currentPiece.shape[rowIndex - newPiecePosition.y][
+//                   colIndex - newPiecePosition.x
+//                 ] || cell
+//               : cell
+//           )
+//     );
+
+//     setTetrisBoard(newTetrisBoard);
+//     setCurrentPiece(getRandomPiece());
+//   };
+
+//   const checkCollision = (piece, position, board) => {
+//     return piece.shape.some((row, rowIndex) =>
+//       row.some((cell, colIndex) => {
+//         if (cell) {
+//           const boardRow = position.y + rowIndex + 1;
+//           const boardCol = position.x + colIndex;
+//           // Verificar colisiones con el borde inferior o celdas fijas en el tablero
+//           if (boardRow >= board.length || board[boardRow][boardCol]) {
+//             return true;
+//           }
+//         }
+//         return false;
+//       })
+//     );
+//   };
+//   const getRandomPiece = () => {
+//     const pieces = [
+//       {
+//         shape: [
+//           [1, 1, 1],
+//           [0, 1, 0],
+//         ],
+//       },
+//       {
+//         shape: [
+//           [1, 1],
+//           [1, 1],
+//         ],
+//       },
+//       // ... Agrega más tipos de piezas según tus necesidades
+//     ];
+
+//     const randomPieceIndex = Math.floor(Math.random() * pieces.length);
+//     const newPiece = pieces[randomPieceIndex];
+
+//     // Posición inicial aleatoria en la parte superior del tablero
+//     const initialPosition = {
+//       x: Math.floor(
+//         Math.random() * (tetrisBoard[0].length - newPiece.shape[0].length + 1)
+//       ),
+//       y: 0,
+//     };
+
+//     return { shape: newPiece.shape, position: initialPosition };
+//   };
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       const newPiecePosition = {
+//         ...currentPiece.position,
+//         y: currentPiece.position.y + 1,
+//       };
+
+//       // Verificar colisiones con el tablero
+//       const pieceCollides = checkCollision(
+//         currentPiece,
+//         newPiecePosition,
+//         tetrisBoard
+//       );
+
+//       if (!pieceCollides) {
+//         setCurrentPiece((prevPiece) => ({
+//           ...prevPiece,
+//           position: newPiecePosition,
+//         }));
+//       } else {
+//         handleCollision(newPiecePosition);
+//       }
+//     }, 500);
+
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, [tetrisBoard, currentPiece]);
+//   useEffect(() => {
+//     document.addEventListener("keydown", handleKeyDown);
+//     return () => {
+//       document.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, []);
+//   useEffect(() => {
+//     drawBoard();
+//   }, [tetrisBoard, currentPiece]);
+
+//   return (
+//     <canvas
+//       ref={canvasRef}
+//       width={270}
+//       height={300}
+//       className="border-2 border-gray-800"
+//     ></canvas>
+//   );
+// };
+
+// export default TetrisBoard;
